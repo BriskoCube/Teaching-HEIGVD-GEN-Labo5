29ee9bce863dce5df2028a07876dbb113d4c198d
Julien Quartier <julien.quartier@bluewin.ch>
2019-06-07 09:34:02 +0200

    Config for unit testing

---
+++ src/test/TestTests.cpp
@@ -0,0 +1,19 @@
+#include "../main/Customer.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+
+namespace {
+    TEST(StatementTest, NO_RENTAL) {
+        Customer customer("Olivier");
+        string statement = customer.statement();
+
+        EXPECT_EQ(statement, "Rental Record for Olivier\n"
+        "Amount owed is 12\n"
+        "You earned 0 frequent renter points\n");
+
+        cout << statement;
+    }
+}
+
